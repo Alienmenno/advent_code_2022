8b619671b3d2562881bd1193bbf223ff8dbdae17
Alienmenno <>
2022-12-06 20:59:09 +0100

    day 06 parameterized tests

--- day06/day06.cc
+++ day06/day06.cc
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include <fstream>
+#include <tuple>
 #include <filesystem>
 #include <range/v3/all.hpp>
 
@@ -15,7 +16,7 @@ std::string get_file(std::filesystem::path file_path) {
     return s;
 }
 
-Marker find_marker(std::string_view s, std::size_t w_size = 4) {
+Marker find_marker(const std::string_view s, const std::size_t w_size = 4) {
     for (auto [i, w] : ranges::views::enumerate(s | ranges::views::sliding(w_size))) {
         if (std::set<char>(w.begin(), w.end()).size() == w_size)
             return i + w_size;
@@ -23,54 +24,47 @@ Marker find_marker(std::string_view s, std::size_t w_size = 4) {
     return 0;
 }
 
-TEST(Day06, Part1_example_1) {
-    ASSERT_EQ(7, find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+//------------------------------------------------------------------------------
+using TestSet = std::tuple<int, std::string, std::size_t>;
+
+class Example : public testing::TestWithParam<TestSet> {};
+class  Puzzle : public testing::TestWithParam<TestSet> {};
+
+TEST_P(Example, find_marker) {
+    auto [answer, packet, width] = GetParam();
+    ASSERT_EQ(answer, find_marker(packet, width));
 }
 
-TEST(Day06, Part1_example_2) {
-    ASSERT_EQ(5, find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+TEST_P( Puzzle, find_marker) {
+    auto [answer, packet_file, width] = GetParam();
+
+    ASSERT_EQ(answer, find_marker(get_file(packet_file), width));
 }
 
-TEST(Day06, Part1_example_3) {
-    ASSERT_EQ(6, find_marker("nppdvjthqldpwncqszvftbrmjlhg"));
-}
+INSTANTIATE_TEST_SUITE_P(
+    Day06,
+    Example,
+    testing::Values(
+        // Part 1
+        std::tuple( 7, "mjqjpqmgbljsphdztnvjfqwrcgsmlb",     4),
+        std::tuple( 5, "bvwbjplbgvbhsrlpgdmjqwftvncz",       4),
+        std::tuple( 6, "nppdvjthqldpwncqszvftbrmjlhg",       4),
+        std::tuple(10, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",  4),
+        std::tuple(11, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",   4),
+        // Part 2
+        std::tuple(19, "mjqjpqmgbljsphdztnvjfqwrcgsmlb",    14),
+        std::tuple(23, "bvwbjplbgvbhsrlpgdmjqwftvncz",      14),
+        std::tuple(23, "nppdvjthqldpwncqszvftbrmjlhg",      14),
+        std::tuple(29, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14),
+        std::tuple(26, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",  14)
+    )
+);
 
-TEST(Day06, Part1_example_4) {
-    ASSERT_EQ(10, find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
-}
-
-TEST(Day06, Part1_example_5) {
-    ASSERT_EQ(11, find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
-}
-
-TEST(Day06, Part1) {
-    std::string s = get_file("day06/puzzle_input.txt");
-
-    ASSERT_EQ(1356, find_marker(s));
-}
-
-TEST(Day06, Part2_example_1) {
-    ASSERT_EQ(19, find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14));
-}
-
-TEST(Day06, Part2_example_2) {
-    ASSERT_EQ(23, find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14));
-}
-
-TEST(Day06, Part2_example_3) {
-    ASSERT_EQ(23, find_marker("nppdvjthqldpwncqszvftbrmjlhg", 14));
-}
-
-TEST(Day06, Part2_example_4) {
-    ASSERT_EQ(29, find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14));
-}
-
-TEST(Day06, Part2_example_5) {
-    ASSERT_EQ(26, find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14));
-}
-
-TEST(Day06, Part2) {
-    std::string s = get_file("day06/puzzle_input.txt");
-
-    ASSERT_EQ(2564, find_marker(s, 14));
-}
+INSTANTIATE_TEST_SUITE_P(
+    Day06,
+    Puzzle,
+    testing::Values(
+        std::tuple(1356, "day06/puzzle_input.txt",  4), // Part 1
+        std::tuple(2564, "day06/puzzle_input.txt", 14)  // Part 2
+    )
+);
